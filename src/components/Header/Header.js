8c833758384471c7d02224cf973185dde1fb0789
qlirimshala20 <qlirimshala20@gmail.com>
2025-11-08 18:39:59 +0100

    Header completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__left">
+        <img
+          src="assets/images/roi-academy-logo-e1750817553650.png"
+          alt="ROI Academy logo"
+          className="logo"
+        />
+      </div>
+
+      <div className="header__center">
+        <nav className="header__nav">
+          <ul className="nav-top">
+            <li>Programet 6–12 mujore ▾</li>
+            <li>Programet 1–2 mujore</li>
+            <li>Future Generations</li>
+            <li>ROI Kids</li>
+          </ul>
+          <ul className="nav-bottom">
+            <li>Rreth ROI Academy</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="header__right">
+        <button className="apply-btn">Apply Now</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
