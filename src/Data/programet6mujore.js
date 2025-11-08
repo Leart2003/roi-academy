6258a7b4d575da13ad0dd5f4b23f97a77a10f146
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2025-11-08 22:53:58 +0100

    Krijimi i te dhenave per programet 6 mujor

---
+++ src/Data/programet6mujore.js
@@ -0,0 +1,69 @@
+const programet = [
+  {
+    id: 1,
+    imageUrl: "assets/images/PROGRAMIM-2-1536x864.png",
+    emriProgramit: "Programim",
+    koha: "6 muaj",
+    description:
+      "Gjatë këtij programi të specializuar 6-mujor, do të mësoni teknologjitë më të kërkuara në zhvillimin e aplikacioneve web dhe softuerike.",
+    rating: 4.9,
+    numberRatings: "1.2K",
+    methodology: "Trajnim dhe Praktikë",
+    pagesa: "1-12 këste",
+  },
+  {
+    id: 2,
+    emriProgramit: "Digital Marketing",
+    koha: "6 muaj",
+    description:
+      "Ky trajnim është i përsosur për ata që dëshirojnë të ndërtojnë një karrierë të ose të avancojnë në profesionin e tyre aktual.",
+    rating: 4.6,
+    numberRatings: "1.1K",
+    methodology: "Trajnim dhe Praktikë",
+    pagesa: "1-12 këste",
+  },
+  {
+    id: 3,
+    emriProgramit: "Cyber Security",
+    koha: "6 muaj",
+    description:
+      "Programi përfshin module të avancuara që mbulojnë të gjitha aspektet kryesore të sigurisë kibernetike.",
+    rating: 4.7,
+    numberRatings: "900",
+    methodology: "Trajnim dhe Praktikë",
+    pagesa: "1-12 këste",
+  },
+  {
+    id: 4,
+    emriProgramit: "Design & Video",
+    koha: "6 muaj",
+    description:
+      "Gjatë këtij programi të specializuar 6-mujor, do të mësoni dizajn grafik, video editim & animacion.",
+    rating: 4.8,
+    numberRatings: "1.2k",
+    methodology: "Trajnim dhe Praktikë",
+    pagesa: "1-12 këste",
+  },
+  {
+    id: 5,
+    emriProgramit: "FutureGenerations",
+    koha: "1 - 3 vite",
+    description:
+      "Future Generations është një program edukimi 3-vjeçar i ndërtuar për të rinjtë e moshës 14 deri në 19 vjeç",
+    rating: "",
+    numberRatings: null,
+    methodology: "Trajnim dhe Praktikë",
+    pagesa: "1-12 këste",
+  },
+  {
+    id: 6,
+    emriProgramit: "Roi Kids",
+    koha: "6 - 12 muaj",
+    description:
+      "ROI Kids është akademi e specializuar në zhvillimin e aftësive teknologjike për fëmijët e moshës 7 deri në 15 vjeç",
+    rating: 4.8,
+    numberRatings: "750",
+    methodology: "Trajnim dhe Praktikë",
+    pagesa: "1-12 këste",
+  },
+]
