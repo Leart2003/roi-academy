77934104c16145864652d6ee747f74d9fcb6d431
Adnan brahimaj <adnanibrahimajofficial@gmail.com>
2025-11-09 15:46:46 +0100

    Footer style update + added Roi Holding logo to assets/images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,75 +8,139 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab);
 const Footer = () => {
   return (
-    <div className="container">
-      <div className="contact">
-        <div className="logo">
+    <div className="footer-div">
+      <div className="footer-container">
+        <div className="contact">
+          <div className="roi-logo">
+            <img
+              src="/assets/images/roi-academy-logo-e1750817553650.png"
+              alt="ROI Logo"
+              width="100"
+            />
+          </div>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-phone" />
+              <span>+383 48 180 194</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-phone" />
+              <span>+355 68 500 0125</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-phone" />
+              <span>+389 78 243 666</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-envelope" />
+              <span>info@roiacademy.net</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+              <span>Icon Tower, Tirana, Prishtina 10000</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+              <span>Zogu I Blvd, Tiranë 1001, Albania</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+              <span>Str Macedonia, Skopje 1000, N. Macedonia</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+              <span>33 City & Central, Marylebone Lane, England</span>
+            </li>
+          </ul>
+        </div>
+        <div className="programs">
+          <h4>Programet</h4>
+          <a> - 6 muaj</a>
+          <a> - 1-2 muaj</a>
+          <a> - Future Generations</a>
+          <a> - ROI Kids</a>
+        </div>
+        <div className="quick-links">
+          <h4>Quick Links</h4>
+          <a>- Ballina</a>
+          <a>- Programet</a>
+          <a>- Rreth ROI Academy</a>
+          <a>- Ekipi</a>
+          <a>- Blogu</a>
+        </div>
+        <div className="subscribe">
+          <h4>Subscribe</h4>
+          <form>
+            <input className="input-style" placeholder="Email"></input>
+            <button className="submit-button">SUBMIT</button>
+          </form>
+          <h4>ROI Academy is part of:</h4>
           <img
-            src="/roi-academy-logo-e1750817553650.png"
-            alt="ROI Logo"
+            className="roi-holding"
+            src="/assets/images/ROI-LOGO_6-Maj_Final-22-768x339.png"
+            alt="ROI Holding Logo"
             width="100"
           />
+          <nav className="btn-nav">
+            <button
+              className="small-btn"
+              style={{ backgroundColor: "#3b5998" }}
+            >
+              <a href="https://www.facebook.com/roiacademy1" target="_blank">
+                <FontAwesomeIcon
+                  icon="fa-brands fa-facebook"
+                  style={{ color: "#ffffff" }}
+                />
+              </a>
+            </button>
+            <button
+              className="small-btn"
+              style={{ backgroundColor: "#262626" }}
+            >
+              <a href="https://www.instagram.com/roiacademy/" target="_blank">
+                <FontAwesomeIcon
+                  icon="fa-brands fa-instagram"
+                  style={{ color: "#ffffff" }}
+                />
+              </a>
+            </button>
+            <button
+              className="small-btn"
+              style={{ backgroundColor: "#0077b5" }}
+            >
+              <a
+                href="https://www.linkedin.com/company/roi-academy/"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon="fa-brands fa-linkedin"
+                  style={{ color: "#ffffff" }}
+                />
+              </a>
+            </button>
+            <button
+              className="small-btn"
+              style={{ backgroundColor: "#000000" }}
+            >
+              <a href="https://x.com/academy_ro84355" target="_blank">
+                <FontAwesomeIcon
+                  icon="fa-brands fa-x-twitter"
+                  style={{ color: "#ffffff" }}
+                />
+              </a>
+            </button>
+          </nav>
         </div>
-        <ul>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-phone" />
-            <span>+383 48 180 194</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-phone" />
-            <span>+355 68 500 0125</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-phone" />
-            <span>+389 78 243 666</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-envelope" />
-            <span>info@roiacademy.net</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-location-dot" />
-            <span>Icon Tower, Tirana, Prishtina 10000</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-location-dot" />
-            <span>Zogu I Blvd, Tiranë 1001, Albania</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-location-dot" />
-            <span>Str Macedonia, Skopje 1000, N. Macedonia</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-location-dot" />
-            <span>33 City & Central, Marylebone Lane, England</span>
-          </li>
-        </ul>
       </div>
-      <div className="programs">
-        <h4>Programet</h4>
-        <a> - 6 muaj</a>
-        <a> - 1-2 muaj</a>
-        <a> - Future Generations</a>
-        <a> - ROI Kids</a>
+      <div className="copyrights">
+        <p>
+          © Copyrights <strong>ROI Academy</strong>. All rights reserved. –
+          Developed by <strong>Sharp Group</strong>
+        </p>
       </div>
-      <div className="quick-links">
-        <h4>Quick Links</h4>
-        <a>- Ballina</a>
-        <a>- Programet</a>
-        <a>- Rreth ROI Academy</a>
-        <a>- Ekipi</a>
-        <a>- Blogu</a>
-      </div>
-      <div className="subscribe">
-        <h4>Subscribe</h4>
-        <form>
-          <input placeholder="Email"></input>
-          <button className="button"></button>
-        </form>
-      </div>
+      ;
     </div>
   );
-  <div className="copyrights"></div>;
 };
 
 export default Footer;
