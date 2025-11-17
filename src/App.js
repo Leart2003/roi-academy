c25e991f704c11df724757be65eeefc33a79b067
MeritonBerisha100 <meritonberishaa10@gmail.com>
2025-11-16 13:49:10 -1200

    Add AboutRoi component with styling and FontAwesome icons; update package dependencies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import Loginform from "./components/Loginform/Loginform";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import AboutRoi from "./components/AboutRoi/aboutroi";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Loginform />
+       <AboutRoi />
       <Footer />
+     
+
+
     </div>
   );
 }
---
+++ src/components/AboutRoi/aboutroi.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./aboutroi.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
+
+const AboutRoi = () => {
+  return (
+    <div className="about-roi-container">
+      <img src="./assets/images/Roi-photo.png" alt="Roi-logo" />
+
+      <div className="about-roi">
+        <h2>Rreth ROI Academy</h2>
+        <p>
+          Ne ofrojmë programe të avancuara dhe të strukturuara, duke përfshirë
+          Programe 2-mujore dhe Programe 6-mujore, me trajnim dhe praktikë , të
+          cilat ndihmojnë pjesëmarrësit të zhvillojnë aftësi të aplikueshme në
+          tregun vendor dhe ndërkombëtar të punës.
+        </p>
+
+        <div className="about-roi-list">
+          <span>
+            <FontAwesomeIcon
+              icon="fa-solid fa-circle-check"
+              className="check-icon"
+            />
+            Trajnim dhe Certifikim
+          </span>
+
+          <span>
+            <FontAwesomeIcon
+              icon="fa-solid fa-circle-check"
+              className="check-icon"
+            />
+            Praktikë e garantuar me referencë pune
+          </span>
+
+          <span>
+            <FontAwesomeIcon
+              icon="fa-solid fa-circle-check"
+              className="check-icon"
+            />
+            Konsulence të vazhdueshme falas
+          </span>
+
+          <button className="about-roi-btn">Më shumë rreth nesh</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutRoi;
