82ddea4990fb27ece8bdf79dce6920055e6c3ce8
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2025-12-03 20:56:37 +0100

    Adding dropdown list and fixing some bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Programs12Page from "./Pages/Programs12Page";
-import ApplicationForm from "./Pages/ApplicationForm";
-import HomePage from "./Pages/HomePage";
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Programs12Page from "./Pages/Programs12Page"
+import ApplicationForm from "./Pages/ApplicationForm"
+import HomePage from "./Pages/HomePage"
+import Programs6 from "./Pages/Programs6"
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/programet-1-2-mujore" element={<Programs12Page />} />
         <Route path="/application" element={<ApplicationForm />} />
+        <Route path="/Programs6" element={<Programs6 />}></Route>
       </Routes>
-
-      <Footer />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./Header.css";
-import { Link } from "react-router-dom";
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
@@ -18,7 +18,19 @@ const Header = () => {
       <div className="header__center">
         <nav className="header__nav">
           <ul className="nav-top">
-            <li>Programet 6–12 mujore ▾</li>
+            <div class="dropdown">
+              <li className="">
+                <Link to={"/Programs6"}>
+                  <a className="active">Programet 6–12 mujore ▾</a>
+                </Link>
+                <div className="dropdown-content">
+                  <span className="anchor">Të gjitha programet</span>
+                  <span className="anchor">Trajnim për programim</span>
+                  <span className="anchor">Trajnim për Digjital Marketing</span>
+                  <span className="anchor">Trajnim për Siguri Kibernetike</span>
+                </div>
+              </li>
+            </div>
 
             <Link to="/programet-1-2-mujore">
               <li className="active">Programet 1–2 mujore</li>
@@ -37,7 +49,7 @@ const Header = () => {
         <button className="apply-bttn">Apply Now</button>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
