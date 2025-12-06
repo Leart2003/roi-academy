6061ab058fe938372970278b377b84d1242a2dcb
MeritonBerisha100 <meritonberishaa10@gmail.com>
2025-12-05 13:59:39 -1200

    Implement routing and new pages; add application form and styles

---
+++ src/Pages/Programs6.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header"
+
+const Programs6 = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Programs6
---
+++ src/Pages/StatsSection.js
@@ -0,0 +1,34 @@
+import "./Programs12Page.css";
+
+const StatsSection = () => {
+  return (
+    <div className="stats-section">
+      <div className="stat-box">
+        <h2>+6000</h2>
+        <p>Studentë të trajnuar</p>
+      </div>
+
+      <div className="stat-box">
+        <h2>95%</h2>
+        <p>Studentë të punësuar</p>
+      </div>
+
+      <div className="stat-box">
+        <h2>42</h2>
+        <p>Trajnerë të punësuar</p>
+      </div>
+
+      <div className="stat-box">
+        <h2>6</h2>
+        <p>Vite nga themelimi</p>
+      </div>
+
+      <div className="stat-box">
+        <h2>3</h2>
+        <p>Shtete me prezencë fizike</p>
+      </div>
+    </div>
+  );
+};
+
+export default StatsSection;
