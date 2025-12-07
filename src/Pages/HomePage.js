09e4425c3d7f6a1618b2a015ba3724f6d1f1df5e
qlirimshala20 <qlirimshala20@gmail.com>
2025-12-07 12:30:08 +0100

    add loyout page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,6 @@
 import Blog from "../components/Blog/Blog";
 import Loginform from "../components/Loginform/Loginform";
-import Header from "../components/Header/Header";
 import ImageSlider from "../components/ImageSlider/ImageSlider";
-import Footer from "../components/Footer/Footer";
 import ProgramsSection from "../components/Programs/ProgramsSection";
 import MonthlyProgram from "../components/MonthlyProgram/MonthlyProgram";
 import OurTeam from "../components/OurTeam/OurTeam";
@@ -11,7 +9,6 @@ import StudentFeedback from "../components/StudentFeedback/StudentFeedback";
 function HomePage() {
   return (
     <div>
-      <Header />
       <Loginform />
       <MonthlyProgram />
       <ProgramsSection />
@@ -19,7 +16,6 @@ function HomePage() {
       <OurTeam />
       <Blog />
       <ImageSlider />
-      <Footer />
     </div>
   );
 }
