533fef0b0a19607f74f2acfb8ce8fd64fcd7967a
ADNprog <adnanibrahimajofficial@gmail.com>
2025-12-16 13:44:29 +0100

    initial responsive style for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -8,6 +9,12 @@ const Header = () => {
   const handleApplyClick = () => {
     navigate("/application");
   };
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+  const toggleMobileMenu = () => {
+    console.log("Clicked", isMobileMenuOpen);
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
 
   return (
     <header className="header">
@@ -20,57 +27,70 @@ const Header = () => {
           />
         </Link>
       </div>
-
-      <div className="header__center">
-        <nav className="header__nav">
-          <ul className="nav-top">
-            <div className="dropdown">
+        <div className="header__center">
+          <nav className={`header__nav ${isMobileMenuOpen ? "active" : ""}`}>
+            <ul className={isMobileMenuOpen ? "active" : ""}>
               <li>
-                <Link to={"/Programet-6-mujore"} className="active">
-                  Programet 6-12 mujore ▾
-                </Link>
-                <div className="dropdown-content">
-                  <Link to="/Programet-6-mujore" className="active">
-                    <span className="anchor">Të gjitha programet</span>
+              <div className="dropdown">
+                
+                  <Link to={"/Programet-6-mujore"} className="active">
+                    Programet 6-12 mujore ▾
                   </Link>
-                  <Link to="/training-programming" className="active">
-                    <span className="anchor">Trajnim për programim</span>
-                  </Link>
-                  <Link to="/digital-marketing" className="active">
-                    <span className="anchor">
-                      Trajnim për Digital Marketing
-                    </span>
-                  </Link>
-                  <Link to={"/CyberSecurity"}>
-                    <span className="anchor">
-                      Trajnim për Siguri Kibernetike
-                    </span>
-                  </Link>
-                </div>
+                  <div className="dropdown-content">
+                    <Link to="/Programet-6-mujore" className="active">
+                      <span className="anchor">Të gjitha programet</span>
+                    </Link>
+                    <Link to="/training-programming" className="active">
+                      <span className="anchor">Trajnim për programim</span>
+                    </Link>
+                    <Link to="/digital-marketing" className="active">
+                      <span className="anchor">
+                        Trajnim për Digital Marketing
+                      </span>
+                    </Link>
+                    <Link to={"/CyberSecurity"}>
+                      <span className="anchor">
+                        Trajnim për Siguri Kibernetike
+                      </span>
+                    </Link>
+                  </div>
+              </div>
+                </li>
+
+              <li className="active">
+                <Link to="/programet-1-2-mujore">Programet 1–2 mujore</Link>
               </li>
-            </div>
+  <li>
+              <Link to="/Future-generation">
+                Future Generations
+              </Link>
+              </li>
+              <li>
+              <Link to={"/Roi-Kids"}>
+                ROI Kids
+              </Link>
+              </li>
+              <li>
+              <Link to={"/Rreth-Roit"}>
+                Rreth ROI Academy
+              </Link>
+              </li>
+              <li className="apply-btn-mobile">
+                <button className="apply-bttn" onClick={handleApplyClick}>
+                  Apply Now
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
-            <li className="active">
-              <Link to="/programet-1-2-mujore">Programet 1–2 mujore</Link>
-            </li>
-
-            <Link to="/Future-generation">
-              <li>Future Generations</li>
-            </Link>
-            <Link to={"/Roi-Kids"}>
-              <li>ROI Kids</li>
-            </Link>
-            <Link to={"/Rreth-Roit"}>
-              <li>Rreth ROI Academy</li>
-            </Link>
-          </ul>
-        </nav>
-      </div>
-
-      <div className="header__right">
-        <button className="apply-bttn" onClick={handleApplyClick}>
-          Apply Now
-        </button>
+        <div className="header__right">
+          <button className="apply-bttn" onClick={handleApplyClick}>
+            Apply Now
+          </button>
+        </div>
+      <div className="header__mobile__icon" onClick={toggleMobileMenu}>
+        <FontAwesomeIcon icon="fa-solid fa-bars" className="mobile-icon"/>        
       </div>
     </header>
   );
