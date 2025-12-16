aac4428b3d75454e2cafd1175961e237d48c2f41
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2025-12-16 18:06:07 +0100

    Adding neww changes!!!!!

--- src/components/ProgramForm/ProgramForm.js
+++ src/components/ProgramForm/ProgramForm.js
@@ -6,28 +6,28 @@ const ProgramForm = () => {
         <div className="row">
           <div className="field">
             <label>Emër</label>
-            <input type="text" />
+            <input className="input" type="text" />
           </div>
 
           <div className="field">
             <label>Mbiemër</label>
-            <input type="text" />
+            <input className="input" type="text" />
           </div>
         </div>
 
         <div className="field full">
           <label>Numri telefonit</label>
-          <input type="text" />
+          <input className="input" type="text" />
         </div>
 
         <div className="field full">
           <label>Email</label>
-          <input type="email" />
+          <input className="input" type="email" />
         </div>
 
         <div className="field full">
           <label>Message</label>
-          <textarea rows="4"></textarea>
+          <textarea className="textarea" rows="4"></textarea>
         </div>
 
         <button type="submit" className="submit-btn">
